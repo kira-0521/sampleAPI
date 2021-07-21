6b202a328eb23c211c4290bfec2db7e132e4eacf
kira-0521 <kiratanaka0521@gmail.com>
2021-07-21 16:51:12 +0900

    feat: first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ hello }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      hello: "hello world",
+    };
+  },
+};
+</script>
+
+<style>
+h1 {
+  font-size: 32px;
+}
+</style>
