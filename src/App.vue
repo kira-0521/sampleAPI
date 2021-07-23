18c93e9999528c0b1f4abe3150c1a9ab0f200aa2
kira-0521 <kiratanaka0521@gmail.com>
2021-07-23 12:08:21 +0900

    feat: 取得データの中身を表示

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    {{ info }}
+    <h1>楽天から検索</h1>
+
+    <section class="error" v-if="errored">
+      <p>
+        We're sorry, we're not able to retrieve this information at the moment,
+        please try back later
+      </p>
+    </section>
+
+    <section v-else>
+      <div class="loading" v-if="loading">Loading...</div>
+
+      <div class="books">
+        {{ info }}
+      </div>
+    </section>
   </div>
 </template>
 
@@ -10,7 +25,9 @@ import axios from "axios";
 export default {
   data() {
     return {
-      info: null,
+      info: [],
+      errored: false,
+      loading: true,
     };
   },
   mounted() {
@@ -18,15 +35,23 @@ export default {
       "https://app.rakuten.co.jp/services/api/BooksTotal/Search/20170404";
     const params = {
       applicationId: "1031111983337341334",
-      booksGenreId: "002",
-      keyword: "漫画",
+      booksGenreId: "001",
+      hits: 20,
     };
     axios
       .get(baseURL, { params: params })
       .then((res) => {
-        this.info = res;
+        res.data.Items.reduce((acc, cur) => {
+          acc.push(cur.Item.title);
+          return (this.info = acc);
+        }, []);
+        console.log(this.info);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log(err);
+        this.errored = true;
+      })
+      .finally(() => (this.loading = false));
   },
 };
 </script>
