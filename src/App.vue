068869cc016f756a3c46bfa7a04267242843824b
kira-0521 <kiratanaka0521@gmail.com>
2021-07-23 12:24:01 +0900

    feat: タイトルとイメージを表示

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
     <h1>楽天から検索</h1>
+    <input
+      type="text"
+      v-model="keyword"
+      placeholder="検索キーワード"
+      @keydown.enter="search"
+    />
 
     <section class="error" v-if="errored">
       <p>
-        We're sorry, we're not able to retrieve this information at the moment,
+        We're sorry, we're not able to retrieve this booksrmation at the moment,
         please try back later
       </p>
     </section>
@@ -13,7 +19,12 @@
       <div class="loading" v-if="loading">Loading...</div>
 
       <div class="books">
-        {{ info }}
+        <ul v-for="book in books" :key="book.index">
+          <li>
+            <h3>タイトル: {{ book.title }}</h3>
+            <img :src="book.img" alt="" />
+          </li>
+        </ul>
       </div>
     </section>
   </div>
@@ -25,33 +36,40 @@ import axios from "axios";
 export default {
   data() {
     return {
-      info: [],
+      books: [],
+      keyword: null,
       errored: false,
       loading: true,
     };
   },
-  mounted() {
-    const baseURL =
-      "https://app.rakuten.co.jp/services/api/BooksTotal/Search/20170404";
-    const params = {
-      applicationId: "1031111983337341334",
-      booksGenreId: "001",
-      hits: 20,
-    };
-    axios
-      .get(baseURL, { params: params })
-      .then((res) => {
-        res.data.Items.reduce((acc, cur) => {
-          acc.push(cur.Item.title);
-          return (this.info = acc);
-        }, []);
-        console.log(this.info);
-      })
-      .catch((err) => {
-        console.log(err);
-        this.errored = true;
-      })
-      .finally(() => (this.loading = false));
+  methods: {
+    search(event) {
+      if (event.keyCode !== 13) return;
+      const baseURL =
+        "https://app.rakuten.co.jp/services/api/BooksTotal/Search/20170404";
+      const params = {
+        applicationId: "1031111983337341334",
+        booksGenreId: "001",
+        hits: 20,
+        keyword: this.keyword,
+      };
+      axios
+        .get(baseURL, { params: params })
+        .then((res) => {
+          res.data.Items.reduce((acc, cur) => {
+            acc.push({
+              title: cur.Item.title,
+              img: cur.Item.smallImageUrl,
+            });
+            return (this.books = acc);
+          }, []);
+        })
+        .catch((err) => {
+          console.log(err);
+          this.errored = true;
+        })
+        .finally(() => (this.loading = false));
+    },
   },
 };
 </script>
